e7647a49412477dc7aae24b310b8babd622113df
TanyEm <tanyem@protonmail.com>
2020-12-07 15:04:18 +0200

    Initial Commit

---
+++ Task Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task Tracker
+//
+//  Created by Tatiana Podlesnykh on 7.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
