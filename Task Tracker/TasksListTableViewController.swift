abec321f3ad4a779c87be1e7575a4b53dca532c0
TanyEm <tanyem@protonmail.com>
2020-12-07 23:45:04 +0200

    Add a checkmark to cells

--- Task Tracker/TasksListTableViewController.swift
+++ Task Tracker/TasksListTableViewController.swift
@@ -12,39 +12,74 @@ class TasksListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let titleFont = [NSAttributedString.Key.font:
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let largeTitleFont = [NSAttributedString.Key.font:
                             UIFont(name: "SF Compact Rounded Bold",
                                    size: 35) ??
-                            UIFont.boldSystemFont(ofSize: 35),]
-        navigationController?.navigationBar.largeTitleTextAttributes = titleFont
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        let attributes = [NSAttributedString.Key.font:UIFont(name: "SF Compact Rounded Medium", size: 19)!]
-        self.editButtonItem.setTitleTextAttributes(attributes, for: .normal)
-        self.navigationItem.rightBarButtonItem = self.editButtonItem
+                            UIFont.boldSystemFont(ofSize: 35)]
+        let titleFont = [NSAttributedString.Key.font:
+                            UIFont(name: "SF Compact Rounded Semibold",
+                                   size: 20) ??
+                            UIFont.boldSystemFont(ofSize: 20)]
+        let editButtonFont = [NSAttributedString.Key.font:
+                                UIFont(name: "SF Compact Rounded Regular",
+                                       size: 19) ??
+                                UIFont.boldSystemFont(ofSize: 19)]
+        
+        navigationController?.navigationBar.largeTitleTextAttributes = largeTitleFont
+        navigationController?.navigationBar.titleTextAttributes = titleFont
+        editButtonItem.setTitleTextAttributes(editButtonFont, for: .normal)
+        navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 100
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        if indexPath.row % 5 == 0 {
+          label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+          label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+          label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+          label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+          label.text = "Eat ice cream"
+        }
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+              cell.accessoryType = .checkmark
+            } else {
+              cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
     /*
     // Override to support conditional editing of the table view.
